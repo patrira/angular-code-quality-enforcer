4abe24b890318b0685127a580255c7035c412507
patrira <iradukundapatrick19@gmail.com>
2024-09-12 11:30:19 +0200

    Adding more component

---
+++ src/app/greetings/greetings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-greetings',
+  standalone: true,
+  imports: [],
+  templateUrl: './greetings.component.html',
+  styleUrl: './greetings.component.css'
+})
+export class GreetingsComponent {
+
+}
